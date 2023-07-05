e563b2476112be1ab48cb20c8f307bb08dcf1843
kyun78 <nkj3248@naver.com>
2023-07-05 19:44:07 +0900

    test3

---
+++ utility.py
@@ -0,0 +1,11 @@
+def add(alpha: int, beta: int)->int:
+    return alpha+beta
+
+def subtract(alpha: int, beta: int)->int:
+    return alpha-beta
+
+def multiply(alpha: int, beta: int)->int:
+    return alpha*beta
+
+def divide(alpha: int, beta: int)->tuple:
+    return alpha//beta, alpha %beta
